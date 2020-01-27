48a5c44a59b31edddf337312e8faee4a6f70ccbb
TheCollinZRusty <36445882+TheCollinZRusty@users.noreply.github.com>
2020-01-27 16:06:31 +0000

    Commit

--- app/src/main/java/ie/wit/markit/ui/home/HomeViewModel.kt
+++ app/src/main/java/ie/wit/markit/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Clonakilty Information"
     }
     val text: LiveData<String> = _text
 }
